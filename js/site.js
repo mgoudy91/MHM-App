7a0dfd99a2bfef8e8d2dd4ad3020058787b5710c
Mitch Goudy <mgoudy91@gmail.com>
2013-12-12 12:38:23 -0700

    New slider, style finishing, comments. This should be final

--- js/site.js
+++ js/site.js
@@ -1,23 +1,25 @@
 /* On Ready*/
 $(document).ready(function() {
 
-    // Init vars
+    /* Init vars */
     var powered = false;
     var minutes = true;
     var time = 0;
+    const MAX_TIME = 999;
 
-    // Helper/format functs
+    /* Helper/format functions */
+    // Pad number to 4 digits
     function pad(num, size) {
         var s = num+"";
         while (s.length < size) s = "0" + s;
         return s;
     }
-    function h2m(min) {
+    // Minutes to hours formatting
+    function m2h(min) {
         return (min/60).toFixed(2);
     }
 
-
-    // Main update function
+    /* Main update function */
     function update_time () {
         var time_str;
         if (minutes) {
@@ -26,25 +28,32 @@ $(document).ready(function() {
         }
         else{
             $('#clock_unit').html("HRS");
-            time_str = pad(h2m(time),4);
+            time_str = pad(m2h(time),4);
         }
-        $('#clock_txt').html(time_str);
+        //Update each digit in display
+        $('#dig_1').html(time_str.substring(0,1));
+        $('#dig_2').html(time_str.substring(1,2));
+        $('#dig_3').html(time_str.substring(2,3));
+        $('#dig_4').html(time_str.substring(3,4));
+        $( "#slider" ).slider( "option", "value", time );
     }
 
-    // Handlers
+    /* Handlers */
     $('#button_power').click(function() {
         if(powered){
+            // Turn off
             $("#clock_txt").animate({opacity: 0.0}, 0);
             $("#clock_unit").animate({opacity: 0.0}, 0);
         }
         else{
+            // Turn on
             $("#clock_txt").animate({opacity: 1.0}, 200);
             $("#clock_unit").animate({opacity: 1.0}, 200);
         }
         powered = !powered;     
     });
     $('#button_plus').click(function() {
-        if (time < 9999 && powered) {
+        if (time < MAX_TIME && powered) {
             time++;
             update_time();
         };
@@ -61,8 +70,19 @@ $(document).ready(function() {
         update_time();
     });
 
-    //Initialize clock
+    /*Initialize app */
+    // Slider widget
+    $( "#slider" ).slider({
+        range: "min",
+        min: 0,
+        max: MAX_TIME,
+        value: 0,
+        slide: function( event, ui ) {
+            time = ui.value;
+            update_time();
+        }
+    });
+    // First time update
     update_time();
-
 });
 
